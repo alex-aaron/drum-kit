1f535b4b15108581cc880a4d9e99507a36b8788b
Alex Aaron <alexaaron@Operations-MacBook-Air.local>
2022-07-20 15:55:57 -0500

    added on click event listeners to drum buttons

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// window.addEventListener('load', () => {
+//   let drum1 = document.getElementById('w');
+// })
+
+document.getElementById('w').addEventListener('click', handleClick);
+
+function handleClick(){
+  console.log('clicked!');
+}
